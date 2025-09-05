fb2c4f0f69e23085000b5cf9a511f795c876abd6
SilverLatios <jxxthomas7@gmail.com>
2025-09-05 18:48:18 -0400

    Added initial stubs

--- src/network/APIHandler.java
+++ src/network/APIHandler.java
@@ -1,5 +1,13 @@
 package network;
+import org.json.*;
 
 public class APIHandler {
-    
+    private JSONObject getCourse(String subject, int number, int term, String session, String campus) {
+        return new JSONObject();
+    }
+
+    // i.e.: subject="CPSC"; number=100 term=2 session="W" campus="V"
+    public boolean isCourseReady(String subject, int number, int term, String session, String campus) {
+        return false;
+    }
 }
